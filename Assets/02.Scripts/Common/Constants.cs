733f1d85c999905d64abf8eece96a55daac768a6
SiaKim <kimsia951@gmail.com>
2026-08-19 21:12:30 +0900

    Init Files

---
+++ Assets/02.Scripts/Common/Constants.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Constants
+{
+    public int MaxPower = 100;
+
+    public enum Values
+    {
+        // 권력
+        Power,
+        
+        // 성격수치
+        Mercy,
+        Ambition,
+        Cunning,
+        Boldness,
+        Lust,
+        
+        // 욕구수치
+        Hunger,
+        Content,
+        Sleep,
+    }
+}
---
+++ Assets/02.Scripts/Sim.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Sim : MonoBehaviour
+{
+    private int power;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Awake()
+    {
+        Initialize();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Initialize()
+    {
+        
+    }
+
+    void ChangeValue(Constants.Values value, int amount)
+    {
+        switch (value)
+        {
+            
+        }
+    }
+}
